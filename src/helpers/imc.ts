fc4737256b22202dd6206ae80353f4542233a5ad
Sidnei Rodrigues <sidnei.c.rodrigues@hotmail.com>
2022-11-20 00:08:56 -0300

    changed property of object levels

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -10,7 +10,7 @@ export const levels: Level[] = [
     { title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [ 0, 18.5 ] },
     { title: 'Normal', color: '#0EAD69', icon: 'up', imc: [ 18.6, 24.9 ] },
     { title: 'Sobrepeso', color: '#E2B035', icon: 'down', imc: [ 25, 30 ] },
-    { title: 'Obesso', color: '#C3423F', icon: 'down', imc: [ 30.1, 99 ] }
+    { title: 'Obeso', color: '#C3423F', icon: 'down', imc: [ 30.1, 99 ] }
 ];
 
 export const calculateImc = (height: number, weight:number) => {
